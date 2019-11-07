f261c3b7bac2cced2eeaeb8b3ccf874496d477ba
Spenser Truex <web@spensertruex.com>
2019-11-06 22:13:32 -0800

    Fish init.

---
+++ config.fish
@@ -0,0 +1,24 @@
+# emacs ansi-term support
+if test -n "$EMACS"
+  set -x TERM eterm-color
+end
+
+# this function may be required by emacs.
+function fish_title
+    true
+end
+
+
+# Aliases
+function emc
+    emacsclient -nca ''
+end
+function wifi
+    cd /home/jose/bin/iw-wrapper/ && ./internet.pl
+end
+function em
+    emacs
+end
+function emq
+    emacs -Q
+end
